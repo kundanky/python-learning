54dbc536cbc23b7812e29804ec8ef4743210f0c8
Kundan Yadav <hopebig871@gmail.com>
2026-08-14 15:40:03 +0530

    updated projects

---
+++ word analyzer.py
@@ -0,0 +1,15 @@
+print("Word analyzer")
+sentence= input("Enter Your Sentence").lower()
+words= sentence.split()
+print(len(words))
+print(words[0:-1])
+search = input("Enter what you're searching for").lower()
+word2= "sakshi"
+if word2 in search:
+	print("Found her")
+else:
+	print("couldn't find her")
+print(f"your {sentence} has found {word2} ")
+print(f"your sentence: {sentence} has {len(words)}words , first : {words[0]} ,last: {words[-1]}")
+
+#"Your sentence has 5 words. First word: hello. Last word: world."
