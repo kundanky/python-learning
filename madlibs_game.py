5335ee4f229eaf39090ddb17736a7480b82ed176
Kundan Yadav <hopebig871@gmail.com>
2026-08-19 02:25:48 +0530

    py update

---
+++ madlibs_game.py
@@ -0,0 +1,9 @@
+# madlibs game 
+#fill random words and create a story
+noun = input("Enter a noun (person):")
+object = input("Enter an object ")
+
+print(f"Today i went to college and saw {noun}:")
+print(f"in an exhibition, i saw  beautiful {object}:")
+print(f"{noun} was holding {object} in arms")
+print(f"but {noun} was prettier than {object}")
